c4f5b58fc11a9f159d0e6d4fe7d8b9a44446d47b
Matheus Pessoa <matheus41.dev@gmail.com>
2026-02-25 22:39:41 -0300

    feat(auth): create ValidateUserUseCase for user authentication

--- src/infra/http/modules/auth/auth.controller.ts
+++ src/infra/http/modules/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, HttpCode, HttpStatus, Post, UseGuards, Request } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthRequestModel } from "./models/authRequestModel";
 
 @Controller()
 export class AuthController {
     @Post('signIn')
     @HttpCode(HttpStatus.OK)
     @UseGuards(AuthGuard('local'))
-    async signIn(@Request() request: any){
+    async signIn(@Request() request: AuthRequestModel){
       return request.user;
     };
 };
--- src/infra/http/modules/user/user.module.ts
+++ src/infra/http/modules/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { CreateUserUseCase } from "src/modules/user/useCases/createUserUseCase/createUserUseCase";
 import { DatabaseModule } from "src/infra/database/database.module";
-import { ValidateUseUserCase } from "src/modules/auth/useCases/validateUserUseCase";
+import { ValidateUseUserCase } from "src/modules/auth/useCases/validateUserUseCase/validateUserUseCase";
 
 @Module({
     imports:[DatabaseModule],
