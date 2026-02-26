d3e61d8a90eed565ae8477d511833254dae5679a
Matheus Pessoa <matheus41.dev@gmail.com>
2026-02-25 22:40:35 -0300

    test: create validateUserUseCase test

---
+++ src/modules/auth/useCases/validateUserUseCase/validateUserUseCase.spec.ts
@@ -0,0 +1,68 @@
+import { User } from "src/modules/user/entities/user.entity";
+import { ValidateUseUserCase } from "./validateUserUseCase";
+import { UserRepositoryInMemory } from "src/modules/user/repositories/UserRepositoryInMemory";
+import { hash } from "bcryptjs"
+import { makeUser } from "src/modules/user/factories/userFactory";
+import { UnauthorizedException } from "@nestjs/common";
+
+let validadeUseUserCase: ValidateUseUserCase;
+let userRepositoryInMemory: UserRepositoryInMemory;
+
+describe("Validate User", () => {
+
+    beforeAll(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory();
+        validadeUseUserCase = new ValidateUseUserCase(userRepositoryInMemory);
+    })
+
+    it("Should be able to return user when credentials are correct", async () => {
+        const userPasswordWithoutEncryption = '123123';
+
+        const user = makeUser({
+            password: await hash(userPasswordWithoutEncryption, 10),
+        });
+
+        userRepositoryInMemory.users = [user];
+
+        const result = await validadeUseUserCase.execute({
+            email: user.email,
+            password: userPasswordWithoutEncryption,
+        });
+
+        expect(result).toEqual(user)
+    });
+
+    it("Should be able to throw error when credentials incorrect", async () => {
+        const userPasswordWithoutEncryption = '123123';
+
+        const user = makeUser({
+            password: await hash(userPasswordWithoutEncryption, 10),
+        });
+
+        userRepositoryInMemory.users = [user];
+
+        expect(async () => {
+            await validadeUseUserCase.execute({
+                email: 'incorrect@gmail.com',
+                password: userPasswordWithoutEncryption,
+            });
+        }).rejects.toThrow(UnauthorizedException)
+    })
+
+    it("Should be able to throw error when credentials incorrect", async () => {
+        const userPasswordWithoutEncryption = '123123';
+
+        const user = makeUser({
+            password: await hash(userPasswordWithoutEncryption, 10),
+        });
+
+        userRepositoryInMemory.users = [user];
+
+        expect(async () => {
+            await validadeUseUserCase.execute({
+                email: user.email,
+                password: "incorrect password",
+            });
+        }).rejects.toThrow(UnauthorizedException)
+    })
+})
