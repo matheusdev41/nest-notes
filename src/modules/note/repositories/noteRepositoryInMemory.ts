d289355f448d8c362f8149024c7d11a0d8db107e
matheusdev41 <matheus41.dev@gmail.com>
2026-03-08 13:43:36 -0300

    feat(note): add in-memory note repository for tests

--- src/modules/note/repositories/noteRepositoryInMemory.ts
+++ src/modules/note/repositories/noteRepositoryInMemory.ts
@@ -1,4 +1,4 @@
-import { Note } from '../entities/node';
+import { Note } from '../entities/note';
 import { NoteRepository } from './noteRepository';
 
 export class NoteRepositoryInMemory implements NoteRepository {
@@ -6,5 +6,17 @@ export class NoteRepositoryInMemory implements NoteRepository {
 
   async create(note: Note): Promise<void> {
     this.notes.push(note);
+  }
+
+  async findById(id: string): Promise<Note | null> {
+    const note = this.notes.find((note) => note.id == id);
+
+    if (!note) return null;
+
+    return note;
+  }
+
+  async delete(id: string): Promise<void> {
+    this.notes = this.notes.filter((note) => note.id != id);
   }
 }
