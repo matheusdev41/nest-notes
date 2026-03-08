c5bca0d5422e3a2f78e44a3ab07f3c5877da187f
matheusdev41 <matheus41.dev@gmail.com>
2026-03-08 13:38:30 -0300

    feat: implements functions create, findById and delete in note repository

--- src/modules/note/repositories/noteRepository.ts
+++ src/modules/note/repositories/noteRepository.ts
@@ -1,5 +1,7 @@
-import { Note } from '../entities/node';
+import { Note } from '../entities/note';
 
 export abstract class NoteRepository {
   abstract create(note: Note): Promise<void>;
+  abstract findById(id: string): Promise<Note | null>;
+  abstract delete(id: string): Promise<void>;
 }
